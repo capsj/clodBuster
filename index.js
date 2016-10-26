d22e314fded68d13244f8c8d6ca4b4d39f330095
NicolasMoreno <nicolas.moreno@ing.austral.edu.ar>
2016-10-26 17:51:08 -0300

    added node.js server, trying to websocket

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by nicolas on 26/10/16.
+ */
+
+var express = require('express');
+var app = express();
+app.use(express.static('www'));
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+
+
+app.get('/', function(req, res){
+  res.sendFile(path.resolve('www/index.html'));
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
