a949ac5fc3ac6da3bbcb600252f46689e20a661f
Jeronimo Carlos <jeronimo.carlos@ing.austral.edu.ar>
2016-11-02 18:52:02 -0300

    mas metodos en scripts.js

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -6,9 +6,84 @@ var moveBackwardsURL = 'https://api.particle.io/v1/devices/2e0039000647343339373
 var moveDownLeftURL = 'https://api.particle.io/v1/devices/2e0039000647343339373536/moveDownLeft?access_token=f913982f971850e14cb30432fad2901aa4456361';
 var stopURL = 'https://api.particle.io/v1/devices/2e0039000647343339373536/stop?access_token=f913982f971850e14cb30432fad2901aa4456361';
 
+function moveUpLeft(argument) {// argument = pin:HIGH || pin:LOW
+    $.ajax({
+        url: moveUpLeftURL,
+        type: 'post',
+        dataType: 'json',
+        data: {args: argument},
+        success: function (data) {
+            communicationSucceeded();
+        },
+        error: function (data) {
+            communicationError();
+        }
+    });
+}
+
 function moveForward(argument) {// argument = pin:HIGH || pin:LOW
     $.ajax({
         url: moveForwardURL,
+        type: 'post',
+        dataType: 'json',
+        data: {args: argument},
+        success: function (data) {
+            communicationSucceeded();
+        },
+        error: function (data) {
+            communicationError();
+        }
+    });
+}
+
+function moveUpRight(argument) {// argument = pin:HIGH || pin:LOW
+    $.ajax({
+        url: moveUpRightURL,
+        type: 'post',
+        dataType: 'json',
+        data: {args: argument},
+        success: function (data) {
+            communicationSucceeded();
+        },
+        error: function (data) {
+            communicationError();
+        }
+    });
+}
+
+function moveDownRight(argument) {// argument = pin:HIGH || pin:LOW
+    $.ajax({
+        url: moveDownRightURL,
+        type: 'post',
+        dataType: 'json',
+        data: {args: argument},
+        success: function (data) {
+            communicationSucceeded();
+        },
+        error: function (data) {
+            communicationError();
+        }
+    });
+}
+
+function moveBackwards(argument) {// argument = pin:HIGH || pin:LOW
+    $.ajax({
+        url: moveBackwardsURL,
+        type: 'post',
+        dataType: 'json',
+        data: {args: argument},
+        success: function (data) {
+            communicationSucceeded();
+        },
+        error: function (data) {
+            communicationError();
+        }
+    });
+}
+
+function moveDownLeft(argument) {// argument = pin:HIGH || pin:LOW
+    $.ajax({
+        url: moveDownLeftURL,
         type: 'post',
         dataType: 'json',
         data: {args: argument},
@@ -28,8 +103,24 @@ function communicationSucceeded(){
 function communicationError(){
     alert("sos muy boludo");
 }
+
 $(document).ready(function(){
+    $("#ButtonUpLeft").click(function(){
+        moveUpLeft("LOW");
+    });
     $("#ButtonUp").click(function(){
         moveForward("LOW");
+    });
+    $("#ButtonUpRight").click(function(){
+        moveUpRight("LOW");
+    });
+    $("#ButtonDownRight").click(function(){
+        moveDownRight("LOW");
+    });
+    $("#ButtonDown").click(function(){
+        moveBackwards("LOW");
+    });
+    $("#ButtonDownLeft").click(function(){
+        moveDownLeft("LOW");
     });
 });
